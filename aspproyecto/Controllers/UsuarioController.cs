a4f0705aa0c743cc802f6e46939279e13b5aff2e
jhonatanacc <jhonatan.acc1031@gmail.com>
2021-05-26 21:39:36 -0500

    cambios conexion base de datos y agregacion de vista usuario

---
+++ aspproyecto/Controllers/UsuarioController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using aspproyecto.Models;
+
+
+namespace aspproyecto.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities () )
+            {
+                return View(db.usuario.ToList());
+            }
+              
+        }
+    }
+}
