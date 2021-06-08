ec0f83c48a1f75a46908490ccc730b4e688a5ac2
jhonatanacc <jhonatan.acc1031@gmail.com>
2021-06-07 21:34:26 -0500

    vista edit

--- aspproyecto/Controllers/UsuarioController.cs
+++ aspproyecto/Controllers/UsuarioController.cs
@@ -13,20 +13,20 @@ namespace aspproyecto.Controllers
         // GET: Usuario
         public ActionResult Index()
         {
-            using (var db = new inventario2021Entities () )
+            using (var db = new inventario2021Entities())
             {
                 return View(db.usuario.ToList());
             }
-              
+
         }
-    public ActionResult create()
+        public ActionResult create()
         {
-            return View () ;
+            return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult create (usuario usuario)
+        public ActionResult create(usuario usuario)
         {
             if (!ModelState.IsValid)
                 return View();
@@ -35,12 +35,14 @@ namespace aspproyecto.Controllers
             {
                 using (var db = new inventario2021Entities())
                 {
+                    usuario.password = UsuarioController.HashSHA1(usuario.password);
                     db.usuario.Add(usuario);
                     db.SaveChanges();
                     return RedirectToAction("Index");
                 }
 
-            }catch(Exception ex)
+            }
+            catch (Exception ex)
             {
                 ModelState.AddModelError("", "Error" + ex);
                 return View();
@@ -49,8 +51,38 @@ namespace aspproyecto.Controllers
 
         }
 
+        public static string HashSHA1(string value)
+        {
+            var sha1 = System.Security.Cryptography.SHA1.Create();
+            var inputBytes = Encoding.ASCII.Getbytes(value);
+            var hash = sha1.ComputeHash(inputBytes);
 
+            var sb = new stringBuilder();
+            for (var i = 0; i < hash.Length; i++)
+
+            {
+                sb.Append(hash[i].ToString("x2"));
+            }
+            return sb.Tostring();
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+
+            try
+            {
+                using(var  db = new inventario2021Entities())
+                {
+                    usuario fundUser = db.usuario.Where(a => a.id == id).FirstOrdefault();
+                    return View(findUser);
+                }
+            }
+            catch  (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
     }
-
-
 }
