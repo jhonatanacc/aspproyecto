dff377c28e8f7c721b66109fb11667fd420f0131
jhonatanacc <jhonatan.acc1031@gmail.com>
2021-06-08 20:24:37 -0500

    completado el crud de usuario

--- aspproyecto/Controllers/UsuarioController.cs
+++ aspproyecto/Controllers/UsuarioController.cs
@@ -122,5 +122,15 @@ namespace aspproyecto.Controllers
 
         }
 
+        public ActionResult Delete(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                var usuario = db.usuario.Find(id);
+                db.usuario.Remove(usuario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
     }
 }
