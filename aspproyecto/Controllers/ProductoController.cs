0fc05acb7205f7445730645a8c8a254205fa596b
jhonatanacc <jhonatan.acc1031@gmail.com>
2021-06-25 14:49:39 -0500

    editar , eliminar, y detalles de producto

--- aspproyecto/Controllers/ProductoController.cs
+++ aspproyecto/Controllers/ProductoController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using aspproyecto.Models;
 
+
 namespace aspproyecto.Controllers
 {
     public class ProductoController : Controller
@@ -13,9 +14,10 @@ namespace aspproyecto.Controllers
         public ActionResult Index()
         {
             using (var db = new inventario2021Entities())
+            {
 
                 return View(db.producto.ToList());
-
+            }
         }
         public static string NombreProveedor(int idProveedor)
         {
@@ -57,5 +59,66 @@ namespace aspproyecto.Controllers
                 return View();
             }
         }
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                producto productoDetalle = db.producto.Where(a => a.id == id).FirstOrDefault();
+                return View(productoDetalle);
+            }
+        }
+        public ActionResult Delete(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                var productDelete = db.producto.Find(id);
+                db.producto.Remove(productDelete);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    producto producto = db.producto.Where(a => a.id == id).FirstOrDefault();
+                    return View(producto);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(producto productoEdit)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var producto = db.producto.Find(productoEdit.id);
+                    producto.nombre = productoEdit.nombre;
+                    producto.percio_unitario = productoEdit.percio_unitario;
+                    producto.cantidad = productoEdit.cantidad;
+                    producto.descripcion = productoEdit.descripcion;
+                    producto.id_proveedor = productoEdit.id_proveedor;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
     }
 }
