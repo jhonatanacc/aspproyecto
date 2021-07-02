dc8eac2caed2c4752bf5079e1acca8ed306b8072
jhonatanacc <jhonatan.acc1031@gmail.com>
2021-07-01 21:49:49 -0500

    UploadCSV  con su vista y cargue masivo en cliente

--- aspproyecto/Controllers/ClienteController.cs
+++ aspproyecto/Controllers/ClienteController.cs
@@ -113,5 +113,61 @@ namespace aspproyecto.Controllers
                 return RedirectToAction("Index");
             }
         }
+        public ActionResult uploadCSV()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult uploadCSV(HttpPostedFileBase fileForm)
+        {
+            //string para guardar la ruta
+            string filePath = string.Empty;
+
+            //condicion para saber si llego el archivo
+            if (fileForm != null)
+            {
+                //ruta de la carpeta que gurdara el archivo
+                string path = Server.MapPath("~/Uploads/");
+
+                //condicion para saber si la ruta de la carpeta existe
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                //obtener el nombre del archivo
+                filePath = path + Path.GetFileName(fileForm.FileName);
+                //obtener la extension del archivo
+                string extension = Path.GetExtension(fileForm.FileName);
+
+                //guardar el archivo
+                fileForm.SaveAs(filePath);
+
+                string csvData = System.IO.File.ReadAllText(filePath);
+
+                foreach (string row in csvData.Split('\n'))
+                {
+                    if (!string.IsNullOrEmpty(row))
+                    {
+                        var newCliente = new cliente
+                        {
+                            nombre = row.Split(';')[0],
+                            documento = row.Split(';')[1],
+                            email = row.Split(';')[2],
+                           
+                        };
+
+                        using (var db = new inventario2021Entities())
+                        {
+                            db.cliente.Add(newCliente);
+                            db.SaveChanges();
+                        }
+                    }
+                }
+            }
+
+            return View();
+        }
     }
 }
